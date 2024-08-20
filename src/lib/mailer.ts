dcd91ce3146096579125f1c5b20ebc768eb15782
shyam <sonitegss@gmail.com>
2024-08-20 10:45:15 +0530

    make dynamic post

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -1,35 +1,36 @@
 import nodemailer from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
 
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  auth: {
-    user: process.env.EMAIL_USERNAME,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
-
-export async function sendEmail(options: {
-  to: string;
-  subject: string;
-  text: string;
-  html: string;
-  attachments: any[];
-}) {
+export const sendEmail = async ({ email, options }: any) => {
   try {
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: options.to,
+    // create a hased token
+    // const hashedToken = await bcryptjs.hash(userId.toString(), 10)
+
+    // if (emailType === "VERIFY") {
+    //     await User.findByIdAndUpdate(userId,
+    //         {verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 3600000})
+    // } else if (emailType === "RESET"){
+    //     await User.findByIdAndUpdate(userId,
+    //         {forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: Date.now() + 3600000})
+    // }
+
+    var transport = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+      },
+    } as SMTPTransport.Options);
+    const mailOptions = {
+      from: process.env.MAIL_FROM_ADDRESS,
+      to: email,
       subject: options.subject,
-      text: options?.text ? options.text : "",
-      html: options?.html ? options.html : null,
-      attachments: options?.attachments ? options.attachments : [],
-    });
-    console.log("Email sent successfully");
-  } catch (error) {
-    console.error("Error sending email:", error);
+      html: options.html,
+    };
+    const mailresponse = await transport.sendMail(mailOptions);
+    return mailresponse;
+  } catch (error: any) {
+    throw new Error(error.message);
   }
-}
+};
